bb8a790e79291fda11ac02192481502519f27016
Janice <janicewang487@gmail.com>
2026-02-08 03:45:30 -0500

    initial styling + home tab

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 setActive = (event, currentElement) => {
-    event.preventDefault();
+    //event.preventDefault();
 
     // remove 'active' class from the previously active element
     const currentActive = document.querySelector('.active');
     if (currentActive) {
-    currentActive.classList.remove('active');
+        currentActive.classList.remove('active');
     }
 
     // add 'active' class to the clicked element
