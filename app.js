d0cad306a961f782439029709ddf532fd383b213
Janice <janicewang487@gmail.com>
2026-02-17 00:37:54 -0500

    scaling stuff + basic hamburger menu

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-setActive = (event, currentElement) => {
-    //event.preventDefault();
-
-    // remove 'active' class from the previously active element
+/* Changes active tab from navigation bar. */
+setActive = (currentElement) => {
     const currentActive = document.querySelector('.active');
     if (currentActive) {
         currentActive.classList.remove('active');
     }
 
-    // add 'active' class to the clicked element
     currentElement.classList.add('active');
+}
+
+/* Toggles display of the hamburger menu. */
+toggleNav = () => {
+    const nav = document.getElementById('vertical-nav-container');
+    const display = window.getComputedStyle(nav).display;
+
+    if (display == 'none') {
+        nav.style.display = 'block';
+    } else if (display == 'block') {
+        nav.style.display = 'none';
+    }
+}
+
+/* Closes the hamburger menu. */
+closeNav = () => {
+    const nav = document.getElementById('vertical-nav-container');
+    nav.style.display = 'none';
 }
