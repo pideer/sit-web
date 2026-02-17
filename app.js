edfb844b3ed0f163d516e771634e41b0eb360b1b
Janice <janicewang487@gmail.com>
2026-02-17 16:00:09 -0500

    styling changes + active tab changes on scroll

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+/* Changes active tab as you scroll. */
+const tabs = document.querySelectorAll('.tab');
+const sections = document.querySelectorAll('section');
+
+let currSection = 'home';
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        if (window.scrollY >= section.offsetTop - 80) {
+            currSection = section.id;
+        }
+    });
+    
+    tabs.forEach(tab => {
+        if (tab.href.includes(currSection)) {
+            document.querySelector('.active').classList.remove('active');
+            tab.classList.add('active');
+        }
+    });
+});
+
 /* Changes main text based on date. */
 document.addEventListener("DOMContentLoaded", () => {
     const header = document.getElementById("home-header");
@@ -5,7 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const today = new Date();
     const beforeDate = new Date(today.getFullYear(), 0, 1); // jan 1
-    const afterDate = new Date(today.getFullYear(), 3, 15) // apr 15
+    const afterDate = new Date(today.getFullYear(), 3, 15); // apr 15
     today.setHours(0, 0, 0, 0);
     beforeDate.setHours(0, 0, 0, 0);
     afterDate.setHours(0, 0, 0, 0);
@@ -14,8 +34,8 @@ document.addEventListener("DOMContentLoaded", () => {
         header.innerHTML = "Confident Accounting for Business and Personal Clients";
         body.innerHTML = "Reliable bookkeeping, payroll, and tax services for businesses and individuals. Get started today!";
     } else {
-        header.innerHTML = `Get Ahead of the ${today.getFullYear()} Tax Season`
-        body.innerHTML = `Prepare for the upcoming ${today.getFullYear()} tax year and contact us to get started!`
+        header.innerHTML = `Get Ahead of the ${today.getFullYear()} Tax Season`;
+        body.innerHTML = `Prepare for the upcoming ${today.getFullYear()} tax year and contact us to get started!`;
     }
 });
 
@@ -28,7 +48,7 @@ setActive = (currentElement) => {
     }
 
     currentElement.classList.add('active');
-}
+};
 
 /* Toggles display of the hamburger menu. */
 toggleNav = () => {
@@ -40,10 +60,10 @@ toggleNav = () => {
     } else if (display == 'block') {
         nav.style.display = 'none';
     }
-}
+};
 
 /* Closes the hamburger menu. */
 closeNav = () => {
     const nav = document.getElementById('vertical-nav-container');
     nav.style.display = 'none';
-}
+};
